df6fca74d335fc25a704a37d9dcbaadbe8f45a0e
datpter <115960125+datpter@users.noreply.github.com>
2023-07-19 08:22:13 +0700

    Add project files.

---
+++ CodeFirts1/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using CodeFirts1.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CodeFirts1.Data
+{
+    public class DataContext : DbContext
+    {
+
+
+        private readonly string _connectionString = "Server=LAPTOP-HP52IBIJ\\SQLEXPRESS; Database=Exam3; User Id=sa; Password=12345678; Trusted_Connection=True; TrustServerCertificate=True;MultipleActiveResultSets=true";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+        public DbSet<Student>Students { get; set; }
+
+    }
+}
---
+++ CodeFirts1/Model/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirts1.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age{ get; set; }
+
+
+    }
+}
